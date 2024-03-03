07c71974f171a04fc835fe41087e0c3cec7979fa
SithuKyaw-Thitsaworks <Sithu.Kyaw@thitsaworks.com>
2024-03-03 11:13:33 +0700

    feat: minor updates

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 12001;
 app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
+  console.info(`Responsive card display page is running at http://localhost:${PORT}`);
 });
